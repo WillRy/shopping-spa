2cbd40ad58b9a4fec1e9856ce43c8e4f4260a3f5
William R <wiribasczky@gmail.com>
2019-06-27 08:27:03 -0300

    Criado modulo para componentes de aprovacao/reprovacao de novos membros

---
+++ src/app/components/pages/chat-inv-user/chat-inv-user-list/chat-inv-user-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-inv-user-list',
+  templateUrl: './chat-inv-user-list.component.html',
+  styleUrls: ['./chat-inv-user-list.component.css']
+})
+export class ChatInvUserListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
